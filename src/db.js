6e33032c2f64116b9a38a4dc02cc9699add8b366
AMVAR-F <angelavillamizar900gmail.com>
2025-07-01 23:07:06 -0400

    dashboard actualizacion, esta feito, sorry, tengo sueño

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import pg from 'pg';
 
 export const pool = new pg.Pool({
-  user: 'postgres',
+  user: 'angela',
   host: 'localhost',
-  password: 'contrasena',
+  password: 'Amvar1234r',
   database: 'Sisbdtur',
   port: 5432,
 });
